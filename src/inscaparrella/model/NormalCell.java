a3af5d9d231cdd97a79a62abb72c2903a4d7ca96
guim <guim.moya@inscaparrella.cat>
2025-03-19 19:26:41 +0100

    Començada la classe NormalCell

--- src/inscaparrella/model/NormalCell.java
+++ src/inscaparrella/model/NormalCell.java
@@ -1,4 +1,49 @@
 package inscaparrella.model;
 
+import inscaparrella.utils.InhabitantType;
+
 public class NormalCell {
+    private InhabitantType inhabitantType;
+
+    public NormalCell() {
+        this.inhabitantType = InhabitantType.NONE;
+    }
+
+    public NormalCell(int x, int y) {
+
+    }
+
+    public NormalCell(NormalCell nc) {
+        this.inhabitantType = nc.inhabitantType;
+    }
+
+    public void setInhabitant(InhabitantType inhabitantType) {
+        this.inhabitantType = inhabitantType;
+    }
+
+    public InhabitantType getInhabitant() {
+        return inhabitantType;
+    }
+
+    public String emitEcho() {
+
+    }
+
+    public boolean isDangerous() {
+
+    }
+
+    @Override
+    public String toString() {
+        String retorn = "L'habitant de la cel·la és: ";
+        if (this.inhabitantType == InhabitantType.NONE) {
+            retorn = "La cel·la és buida";
+        } else if (inhabitantType == InhabitantType.BAT) {
+            retorn += "un ratpenat";
+        } else {
+            retorn += "el Wumpus";
+        }
+        return retorn;
+    }
 }
+
