823e3657b9cd86c73f76c9a71fc81813c16ff610
JoelEspinos <jespinosruiz@gmail.com>
2025-03-20 12:36:23 +0100

    Afegit nova branca, i afegit els atributs de WupusLaberynth.

--- src/inscaparrella/model/WumpusLaberynth.java
+++ src/inscaparrella/model/WumpusLaberynth.java
@@ -1,4 +1,10 @@
 package inscaparrella.model;
 
+import java.util.ArrayList;
+
 public class WumpusLaberynth {
+    private ArrayList<ArrayList<Cell>> laberynth;
+    private int[] ppos;
+    private int[] wumpuspos;
+    private int[] batspos;
 }
