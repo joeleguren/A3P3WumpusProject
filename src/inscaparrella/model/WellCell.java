8e427a9ab384962135f5aa125347f7e2cddc3b7f
JoelEspinos <jespinosruiz@gmail.com>
2025-03-20 19:02:42 +0100

    Classe WellCell acabada.

--- src/inscaparrella/model/WellCell.java
+++ src/inscaparrella/model/WellCell.java
@@ -1,9 +1,36 @@
 package inscaparrella.model;
 
-public class WellCell {
+import inscaparrella.utils.CellType;
+
+public class WellCell extends Cell{
 
     public WellCell() {
         super();
+        super.ctype = CellType.WELL;
+    }
 
+    public WellCell(int x, int y) {
+        super(x, y);
+        super.ctype = CellType.WELL;
+    }
+
+    public WellCell(WellCell wc) {
+        super(wc);
+    }
+
+    @Override
+    public String emitEcho() {
+        return "FFFFfffff...";
+    }
+
+    @Override
+    public boolean isDangerous() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Tipus " + this.ctype;
     }
 }
+
