c2a1564aaf6e298fcdf1faef6d8ed5d6de43de03
joeleguren <joeleguren7@gmail.com>
2025-03-20 13:19:44 +0100

    Afegeixo Player

--- src/inscaparrella/model/Cell.java
+++ src/inscaparrella/model/Cell.java
@@ -1,4 +1,73 @@
 package inscaparrella.model;
 
-public class Cell {
+import inscaparrella.utils.CellType;
+
+import java.util.Objects;
+
+public abstract class Cell {
+    private int row;
+    private int col;
+    protected CellType ctype;
+    protected boolean open;
+
+    public Cell() {
+        this.row = -1;
+        this.col = -1;
+        this.ctype = CellType.NONE;
+    }
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Cell(Cell cell) {
+        this.row = cell.row;
+        this.col = cell.col;
+        this.ctype = cell.ctype;
+    }
+
+    public CellType getCtype() {
+        return ctype;
+    }
+
+    public void setCtype(CellType ctype) {
+        this.ctype = ctype;
+    }
+
+    public boolean isOpen() {
+        if (open) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void openCell() {
+        this.open = true;
+    }
+
+    public abstract String emitEcho();
+
+    public abstract boolean isDangerous();
+
+
+    @Override
+    public String toString() {
+        return "Cel·la [" + row + ", " + col + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return row == cell.row && col == cell.col && open == cell.open && ctype == cell.ctype;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col, ctype, open);
+    }
 }
--- src/inscaparrella/model/PowerUpCell.java
+++ src/inscaparrella/model/PowerUpCell.java
@@ -1,4 +1,41 @@
 package inscaparrella.model;
 
-public class PowerUpCell {
+import inscaparrella.utils.CellType;
+import inscaparrella.utils.PowerUp;
+
+import java.util.Random;
+
+public class PowerUpCell extends Cell{
+
+    PowerUp power;
+    // Entre els poders random s'ha d'incloure el NONE???
+
+    public PowerUpCell() {
+        super();
+        super.ctype= CellType.POWERUP;
+        Random r = new Random();
+
+        PowerUp[] types = PowerUp.values();
+
+        int num = r.nextInt(0, types.length);
+
+        this.power = types[num];
+    }
+
+    public PowerUpCell(int row, int col) {
+        super(row, col);
+        super.ctype= CellType.POWERUP;
+
+        Random r = new Random();
+
+        PowerUp[] types = PowerUp.values();
+
+        int num = r.nextInt(0, types.length);
+
+        this.power = types[num];
+
+    }
 }
+
+
+
