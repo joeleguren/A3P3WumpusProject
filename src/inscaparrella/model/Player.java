554b95d3dbe8a26255ebcc5e9212d8ab0aa7cf2e
joeleguren <joeleguren7@gmail.com>
2025-03-20 12:47:25 +0100

    Afegeixo Player

--- src/inscaparrella/model/Player.java
+++ src/inscaparrella/model/Player.java
@@ -1,4 +1,28 @@
 package inscaparrella.model;
 
 public class Player {
+
+    int row;
+    int col;
+    int powers[];
+
+    public Player() {
+        this.row = -1;
+        this.col = -1;
+        this.powers = new int[2];
+    }
+
+    public Player(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.powers = new int[]{2, 0};
+    }
+
+    public void setStartingCell(int row, int col) {
+        
+    }
+
+
+
+
 }
